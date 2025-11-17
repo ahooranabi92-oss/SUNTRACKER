33e518bc536a27a0c55ecb5cc808f60e8b8851bc
ahooranabi92-oss <ahoora.nabi92@gmail.com>
2025-11-17 10:36:40 +0100

    Settings
    Problems fixed

--- sun tracker/sun tracker/SettingsView.swift
+++ sun tracker/sun tracker/SettingsView.swift
@@ -2,10 +2,10 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-       
+        
         NavigationStack {
             List {
-               
+                
                 Section {
                     Button(action: {
                         
@@ -24,73 +24,73 @@ struct SettingsView: View {
                     .listRowBackground(Color.gray.opacity(0.2))
                 }
                 
-               
-                Section(header: Text("Settings")) {
                 
+                Section(header: Text("Settings")) {
+                    
                     SettingsRow(iconName: "clock", title: "Date and Time", detail: "24-h")
                     
-                 
+                    
                     SettingsRow(iconName: "ruler", title: "Units", detail: "Metric")
                     
-                  
+                    
                     SettingsRow(iconName: "globe", title: "Geographic Coordinates", detail: "DDD")
                     
-                  
-                    SettingsRow(iconName: "eye", title: "Appearance", detail: "Automatic")
                     
-                   
+                    
+                    SettingsRow(iconName: "eye", title: "Appearance", detail: "Automatic")
+                }
+                
+                Section() {
+                    
                     SettingsRow(iconName: "gearshape", title: "Allow to Access", detail: nil)
                     
-                 
+                    
                     SettingsRow(iconName: "a.magnify", title: "Language", detail: nil)
                 }
                 
-              
+                
                 Section(header: Text("Support")) {
-                
-                    SettingsRow(iconName: "message", title: "Feedback", detail: nil)
                     
-                 
-                    SettingsRow(iconName: "questionmark.circle", title: "Help", detail: nil)
+                    SettingsRow(iconName: "person.fill.questionmark", title: "Help & Support", detail: nil)
                     
-                    SettingsRow(iconName: "star", title: "Rate App", detail: nil)
-                                    }
-                SettingsRow(iconName: "list.bullent.ident", title: "Terms", detail: nil)
-            }
-            .listStyle(.insetGrouped)
-            .navigationTitle("Settings")        }
+                    
+                    
+                }
+                .listStyle(.insetGrouped)
+                .navigationTitle("Settings")        }
+        }
     }
-}
-
-struct SettingsRow: View {
-    let iconName: String
-    let title: String
-    let detail: String?
     
-    var body: some View {
-       
-        NavigationLink(destination: Text("\(title) ...")) {
-            HStack {
-                Image(systemName: iconName)
-                    .frame(width: 24, alignment: .leading)
-                    .foregroundColor(.secondary)
-                
-                Text(title)
-                
-                Spacer()
-                
-                if let detail = detail {
-                    Text(detail)
+    struct SettingsRow: View {
+        let iconName: String
+        let title: String
+        let detail: String?
+        
+        var body: some View {
+            
+            NavigationLink(destination: Text("\(title) ...")) {
+                HStack {
+                    Image(systemName: iconName)
+                        .frame(width: 24, alignment: .leading)
                         .foregroundColor(.secondary)
+                    
+                    Text(title)
+                    
+                    Spacer()
+                    
+                    if let detail = detail {
+                        Text(detail)
+                            .foregroundColor(.secondary)
+                    }
                 }
             }
         }
     }
-}
-
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-            .preferredColorScheme(.dark)
+    
+    struct SettingsView_Previews: PreviewProvider {
+        static var previews: some View {
+            SettingsView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
